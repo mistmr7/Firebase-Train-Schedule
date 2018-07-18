c535631e3c837be681f3a59388bcf7f3c2ef6fa7
mistmr7 <michael.mistarz@gmail.com>
2018-07-18 11:52:23 -0400

    commit changes, nearly finished, need to debug

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-// Initialize Firebase
+
     var config = {
         apiKey: "AIzaSyBnGO0TJakV0GqoO-K93ta1Z_ukpQXXidU",
         authDomain: "train-schedule-c4e61.firebaseapp.com",
@@ -12,18 +12,68 @@ $(document).ready(function() {
     firebase.initializeApp(config);
 
     let database = firebase.database()
-    let clickCounter = 0
   
     $('#formSubmit').on('click', function(e){
-        e.preventDefault()
-        console.log(e)
-        clickCounter++
+        e.preventDefault();
+        console.log('clicked');
+  
+        database.ref().push({
+            trainName: $('[name=trainName]').val(),
+            destination: $('[name="destinationName"]').val(),
+            firstTrainTime: $('[name="firstTrainTimeName"]').val(),
+            frequency: $('[name="frequencyName"]').val()
+        });
+  
+        trainSchedule.push({
+            trainName: $('[name=trainName]').val(),
+            destination: $('[name="destinationName"]').val(),
+            firstTrainTime: $('[name="firstTrainTimeName"]').val(),
+            frequency: $('[name="frequencyName"]').val()
+        });
+  
+      document.querySelectorAll('.form-control').innerHTML = ''
+    })
+  
+    database.ref().on('child_added', function (e){
+        var train = e.val();
+          trainSchedule.push(train);
+          renderTrains();
+    })
+  
+  
+  
+  var trainSchedule = [{trainName: "Oregon Express", destination: "Salem", firstTrainTime: "06:00", frequency: 40},
+                        {trainName: "Silver Bullet", destination: "Denver", firstTrainTime: "04:00", frequency: 30},
+                        {trainName: "Raleigh Slowpoke", destination: "Charlotte", firstTrainTime: "08:00", frequency: 180},
+                        {trainName: "NYC Express", destination: "Manhattan", firstTrainTime: "03:30", frequency: 3}]
 
-        database.ref().set({
-          clickCount: clickCounter
-      })
-  })
-})
 
+  renderTrains()
 
+    function renderTrains () {
+        $('tbody').empty();
+        
+        for (var i=0; i < trainSchedule.length; i++) {
+            var row = $("<tr>");
+            var td1 = $("<td>").text(trainSchedule[i].trainName);
+            var td2 = $("<td>").text(trainSchedule[i].destination);
+            var tStart = trainSchedule[i].firstTrainTime;
+            var tFrequency = trainSchedule[i].frequency
+            var tStart = trainSchedule[i].firstTrainTime
+            var tStartConverted = moment(tStart, "HH:mm").subtract(1, 'years')
+            var currentTime = moment()
+            var diffTime = moment().diff(moment(tStartConverted), "minutes")
+            var tRemainder = diffTime % tFrequency
+            var tMinutesTilTrain = tFrequency - tRemainder
+            var nextTrain = moment().add(tMinutesTilTrain, "minutes").format('HH:mm')
+            var td3 = $('<td>').text(tFrequency)
+            var td4 = $('<td>').text(nextTrain)
+            var td5 = $('<td>').text(tMinutesTilTrain)
+        
+            row.append(td1).append(td2).append(td3).append(td4).append(td5);
+            $("tbody").append(row);
+        }
+    }
    
+
+})
